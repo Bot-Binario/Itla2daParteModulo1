6810cf35ff2342b9f606a316a4fdbb02c92aab1b
jose.diaz <jose.diaz@micm.gob.do>
2024-10-24 10:19:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*Leer tres calificaciones y calcular el promedio. 
+ * Si el promedio es igual o superior a siete, 
+ * imprimir un mensaje indicando que el alumno ha aprobado. */
+
+using Itla2daParteModulo1;
+using System;
+
+namespace Showing_Average 
+{
+ class Program 
+    
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Ingrese la primera calificación: ");
+            int.TryParse(Console.ReadLine(), out int First_Calification);
+
+            Console.WriteLine("Ingrese la segunda calificación: ");
+            int.TryParse(Console.ReadLine(), out int Second_Calification);
+
+            Console.WriteLine("ingrese la tercera calificación: ");
+            int.TryParse(Console.ReadLine(), out int Third_Calification);
+
+            {
+                Average_Calculator.Average_Calculation(First_Calification, Second_Calification, Third_Calification);    
+            }
+          
+        }
+    } 
+}
+    
+
+
---
+++ grade average.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Leer tres calificaciones y calcular el promedio. 
+ * Si el promedio es igual o superior a siete, 
+ * imprimir un mensaje indicando que el alumno ha aprobado. */
+
+namespace Itla2daParteModulo1
+{
+    internal class Average_Calculator
+    {
+        /* To create a Funtion we use public/private + the methods + Name of the Funtion + (declare type of variables) */
+        public static void Average_Calculation(int A, int B, int C)
+        {
+            int Califications = A + B + C;
+            int Average = Califications / 3;
+            {
+                if (Average >= 70)
+                {
+                    Console.WriteLine("El Alumno ha aprobado");
+                }
+                else
+                {
+                    Console.WriteLine("El alumno ha reprobado la materia");
+                }
+            }
+
+        }
+
+    }
+}
